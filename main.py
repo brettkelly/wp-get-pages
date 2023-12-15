7e3b2d2a4c777c56e1c893fc13605abdd64df7a5
Brett Kelly <brett@brettkelly.org>
2023-12-15 08:30:41 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+def get_published_pages(domain):
+    api_base = f"http://{domain}/wp-json/wp/v2/pages"
+    params = {
+        'status': 'publish',
+        'per_page': 100,
+        'page': 1
+    }
+    all_pages = []
+
+    headers = {
+        'Content-type': 'application/json',
+        'User-agent': 'brettkelly.org'
+    }
+    while True:
+        response = requests.get(api_base, params=params, headers=headers)
+        if response.status_code != 200:
+            break
+
+        pages = response.json()
+        if not pages:
+            break
+
+        for page in pages:
+            lpath = page['link'].replace(f'https://{domain}','')
+            all_pages.append(lpath)
+
+        params['page'] += 1
+
+    return all_pages
+
+if(len(sys.argv) == 2):
+    published_pages = get_published_pages(sys.argv[1].strip())
+    for url in published_pages:
+        print(url)
